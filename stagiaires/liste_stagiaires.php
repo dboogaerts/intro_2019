0c6cf39b510c62263bdb0e7eb1a57c27bc9038b2
Denis Boogaerts Afelio <d.boogaerts@afelio.be>
2019-12-04 14:52:09 +0100

    ajout de données

--- stagiaires/liste_stagiaires.php
+++ stagiaires/liste_stagiaires.php
@@ -6,11 +6,43 @@ $stagiaires = [
         "sexe" => "M",
         "enfants" => 1,
         "hobbies" => ["VID", "TEN", "PGM"] 
+    ],
+    [
+        "nom" => "Arabia",
+        "prenom" => "Jonathan",
+        "ddn" => "05/10/1993",
+        "sexe" => "M",
+        "enfants" => 0,
+        "hobbies" => ["KAR", "VEL", "PGM","MNG"]
+    ],
+    [
+        "nom" => "RALU",
+        "prenom" => "Renaud",
+        "ddn" => "05/03/1983",
+        "sexe" => "M",
+        "enfants" => 2,
+        "hobbies" => ["BSK", "PIA", "CHT"]
+    ],
+    [
+        "nom" => "Connault", 
+        "prenom" => "Clotilde", 
+        "ddn" => "23/06/1987",
+        "sexe" => "F",
+        "enfants" => 5,
+        "hobbies" => ["GUI", "JDR", "PGM"] 
     ]
 ];
 
 $hobbies = [
     "VID" => "Vidéo",  
     "TEN" => "Tennis",  
-    "PGM" => "Programation",  
+    "PGM" => "Programation", 
+    "KAR" => "Karate",
+    "VEL" => "Velo",
+    "MNG" => "Manga",
+    "BSK" => "Basketball",  
+    "PIA" => "Piano",  
+    "CHT" => "Chant",
+    "GUI" => "Guitare",  
+    "JDR" => "Jeu de rôle",  
 ];
